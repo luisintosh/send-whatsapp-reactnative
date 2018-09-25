b4b969e7d97c851f3775c328758e85cf6a309059
Luis <luismepa@gmail.com>
2018-09-24 20:51:20 -0500

    Create a info tab

--- src/home/tabs/InfoTab.js
+++ src/home/tabs/InfoTab.js
@@ -1,12 +1,32 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {Text} from 'react-native';
-import {} from 'native-base';
+import {Text, View, Linking} from 'react-native';
+import {Card, CardItem, Body, Icon, Button, H3} from 'native-base';
 
 class InfoTab extends Component {
+
+  /**
+   * Open tequilapps.com website
+   */
+  openTequilapps() {
+    Linking.openURL('https://www.tequilapps.com');
+  }
+
   render() {
     return (
-      <Text>InfoTab</Text>
+      <View>
+        <Card>
+          <CardItem>
+            <Body>
+              <H3>Discover more apps in our website.</H3>
+              <Button onPress={this.openTequilapps()} transparent iconLeft>
+                <Icon name="planet"/>
+                <Text>https://www.tequilapps.com</Text>
+              </Button>
+            </Body>
+          </CardItem>
+        </Card>
+      </View>
     );
   }
 }
